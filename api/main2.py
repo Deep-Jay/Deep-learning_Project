2f4c9bfe2e379999e8ec51cc3a2e35f512295fa1
Sayan Khanra <101244867+sskbond007@users.noreply.github.com>
2022-03-12 09:10:28 +0530

    Modified FastAPI server

--- api/main2.py
+++ api/main2.py
@@ -4,10 +4,11 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 app = FastAPI()
 
-MODEL = tf.keras.models.load_model("./potato-disease/saved_models/1/")
+MODEL = tf.keras.models.load_model("../saved_models/1")
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
 @app.get("/ping")
@@ -15,14 +16,26 @@ async def ping():
     return "Hello, I am alive"
 
 def read_file_as_image(data) -> np.ndarray:
-    image = np.array(Image.open(BytesIO(data)))
-    return image
+    image = (Image.open(BytesIO(data)))
+    image = np.array(image.resize((256,256)))
+    # image2 = Image.open(BytesIO(data))
+    return image  #,image2
 
 @app.post("/predict")
 async def predict(
     file: UploadFile = File(...)
 ):
     image = read_file_as_image(await file.read())
+    #plt.imshow(image)
+    #image2.show()
+    #resize_image = image2.resize((256,256))
+    #resize_image.show()
+    # plt.imshow(image)
+    #plt.show()
+    # image2 = image.copy()
+    # image2 = np.resize(image2, (256,256,3))
+    # plt.imshow(image2)
+    # plt.show()
     img_batch = np.expand_dims(image, 0)
 
     predictions = MODEL.predict(img_batch)
